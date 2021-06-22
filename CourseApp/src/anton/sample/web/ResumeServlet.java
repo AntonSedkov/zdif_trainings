329ffff8b4f6c4aaf9ff2ea98126d8a24a2cec70
User from script <S3dk0vAG@gmail.com>
2021-06-22 22:09:10 +0300

    add web facet with servlet
    add web facet with servlet

---
+++ CourseApp/src/anton/sample/web/ResumeServlet.java
@@ -0,0 +1,23 @@
+package anton.sample.web;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * User: Sedkov Anton
+ * Date: 22.06.2021
+ */
+public class ResumeServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        super.doGet(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        super.doPost(request, response);
+    }
+}
